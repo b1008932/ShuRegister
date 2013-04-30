09f044147ecf4494774fe8fd26df5f0fd31b4bfc
root <root@WebArch.localdomain>
2013-04-30 15:20:37 +0100

    Problem with the Domain files

--- ShuRegister/grails-app/domain/uk/ac/shu/webarch/ShuRegister/Student.groovy
+++ ShuRegister/grails-app/domain/uk/ac/shu/webarch/ShuRegister/Student.groovy
@@ -26,12 +26,14 @@ class Student {
     classAtts:'student'
   ]
 
-  /**
-   * TODO:
-   * Make the notes field use the "text" type, and name the column "student_notes" in the database
-   */
+  static mapping = {
+table "Student"
+notes column: "student_notes" , type: "text"
+}
+
   static constraints = {
     fullStudentName(nullable:false, blank:false,maxSize:256);
     studentNumber(nullable:false, blank:false,maxSize:256);
-  }
+      }
 }
+
